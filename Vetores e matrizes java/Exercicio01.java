8e92670fbdaeb0bcb7db7d91c8597829cda3fca2
Pablo <bruli_pablo@hotmail.com>
2021-09-05 20:23:55 -0300

    Primeira parte vetor e matrizes java

---
+++ Vetores e matrizes java/Exercicio01.java
@@ -0,0 +1,31 @@
+package br.com.generation.Java;
+
+import java.util.Scanner;
+
+public class Exercicio01 {
+
+	public static void main(String[] args) {
+
+Scanner ler = new Scanner(System.in);
+		
+		int[] vetor = new int[5]; 
+		int maiorValor = 0, media = 0;
+		
+		
+		for(int i = 0; i <= 4; i++) {
+			
+			System.out.println("Digite o "+ (i + 1) + "º valor.");
+			vetor[i] = ler.nextInt();
+			
+			maiorValor = vetor[i] + maiorValor;
+			media = maiorValor / 5;
+		}
+		System.out.println("O maior valor é: " + maiorValor);
+		System.out.println("E a média é: "+ media);
+		
+		ler.close();
+		
+		
+	}
+
+}
