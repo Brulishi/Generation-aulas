15a06bfeae37ae19ce1518a376883c65e27256a1
Pablo <bruli_pablo@hotmail.com>
2021-10-07 19:07:31 -0300

    [Feature Pablo Task:Finalizando a security no blogpessoal]

--- blogpessoal/src/main/java/org/generation/blogpessoal/seguranca/BasicSegurityConfig.java
+++ blogpessoal/src/main/java/org/generation/blogpessoal/seguranca/BasicSegurityConfig.java
@@ -4,18 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@EnableWebMvc
+@EnableWebSecurity
 public class BasicSegurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
-	private UserDetailsService userDetailsService;
+	private UserDetailsServiceImpl userDetailsService; 
 	
 	@Override 
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
